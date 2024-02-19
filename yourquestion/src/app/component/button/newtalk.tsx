a0af695bf8fbd5bddd6a77256bda9ae707a0ec37
JungYoungSick <junfas1216@gmail.com>
2024-02-19 15:28:58 +0900

    +버튼 클릭 이벤트로 자동 창닫기 코드 설정

--- yourquestion/src/app/component/button/newtalk.tsx
+++ yourquestion/src/app/component/button/newtalk.tsx
@@ -1,32 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import AdminTalkPlus from "./newTalkTogle/adminTalkPlus";
 import NewHeader from "./newTalkTogle/newHeader";
 
 const NewTalk = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(false); // 토글 팝업 상태 관리
   const [isAdminTalkPlusOpen, setIsAdminTalkPlusOpen] = useState(false);
   const [isNewHeaderOpen, setIsNewHeaderOpen] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  // useRef에 대한 타입을 HTMLDivElement | null로 명시적으로 선언합니다.
+  const popupRef = useRef<HTMLDivElement | null>(null);
 
   const handleToggle = () => {
-    setIsPopupOpen(!isPopupOpen); // 토글 상태 토글
-    // 팝업을 닫을 때 AdminTalkPlus와 NewHeader도 닫히도록 설정
-    if (isPopupOpen) {
-      setIsAdminTalkPlusOpen(false);
-      setIsNewHeaderOpen(false);
-    }
+    setIsPopupOpen(!isPopupOpen);
   };
 
   const handleAdminTalkPlusOpen = () => {
     setIsAdminTalkPlusOpen(true);
     setIsNewHeaderOpen(false);
-    setIsPopupOpen(false); // AdminTalkPlus를 열 때 토글 팝업 닫기
+    setIsPopupOpen(false);
   };
 
   const handleNewHeaderOpen = () => {
     setIsAdminTalkPlusOpen(false);
     setIsNewHeaderOpen(true);
-    setIsPopupOpen(false); // NewHeader를 열 때 토글 팝업 닫기
+    setIsPopupOpen(false);
   };
+
+  useEffect(() => {
+    // 'event' 매개 변수의 타입을 MouseEvent로 명시합니다.
+    const handleClickOutside = (event: MouseEvent) => {
+      // 'event.target'을 'Node'로 타입 단언합니다.
+      const target = event.target as Node;
+      if (popupRef.current && !popupRef.current.contains(target)) {
+        setIsPopupOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="relative">
@@ -35,18 +48,21 @@ const NewTalk = () => {
       </button>
 
       {isPopupOpen && (
-        <div className="absolute top-12 left-1/2 transform -translate-x-1/2 w-48 bg-white p-4 rounded-lg shadow-md">
+        <div
+          ref={popupRef}
+          className="absolute top-12 left-1/2 transform -translate-x-1/2 w-48 bg-white p-4 rounded-lg shadow-md"
+        >
           <div
             className="text-center mb-2 cursor-pointer"
             onClick={handleAdminTalkPlusOpen}
           >
-            답변자 기록 추가
+            Admin Talk Plus
           </div>
           <div
             className="text-center mb-2 cursor-pointer"
             onClick={handleNewHeaderOpen}
           >
-            새로운 대화 생성
+            New Header
           </div>
         </div>
       )}
