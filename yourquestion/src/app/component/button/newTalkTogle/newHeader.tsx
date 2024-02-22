d4515fa7101525a176265c8efd2b8b594f0f95cd
JungYoungSick <junfas1216@gmail.com>
2024-02-22 21:43:26 +0900

    웹 개발자도구 상 데이터 출력 확인

--- yourquestion/src/app/component/button/newTalkTogle/newHeader.tsx
+++ yourquestion/src/app/component/button/newTalkTogle/newHeader.tsx
@@ -1,6 +1,21 @@
 // NewHeader.tsx
 import React, { useState } from "react";
 
+function decodeJwt(token: string): any {
+  const base64Url = token.split(".")[1]; // 토큰의 payload 부분을 추출
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload); // payload를 객체로 파싱하여 반환
+}
+
 const NewHeader = ({
   isOpen,
   onClose,
@@ -11,6 +26,16 @@ const NewHeader = ({
   const [title, setTitle] = useState("");
 
   const addTitle = async () => {
+    const token = localStorage.getItem("token"); // 로컬 스토리지에서 토큰 가져오기
+    if (!token) {
+      console.error("토큰이 없습니다.");
+      return;
+    }
+
+    // 토큰에서 userID 추출
+    const decodedToken = decodeJwt(token);
+    console.log("Decoded Token:", decodedToken); // 디코딩된 토큰 전체를 콘솔에 출력
+    console.log("UserID:", decodedToken.userID); // 토큰에서 추출한 userID를 콘솔에 출력
     try {
       const response = await fetch("/newTalk/newHeader/title", {
         method: "POST",
