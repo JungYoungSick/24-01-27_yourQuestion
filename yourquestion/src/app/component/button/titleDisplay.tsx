2ccb43497f41e1f497efc61307579ef05228e2b9
JungYoungSick <junfas1216@gmail.com>
2024-02-23 13:10:51 +0900

    titleDisplay로 인해 생기는 오류 주석처리로 임시 표시제거

--- yourquestion/src/app/component/button/titleDisplay.tsx
+++ yourquestion/src/app/component/button/titleDisplay.tsx
@@ -8,26 +8,26 @@ interface TitleData {
 const TitleDisplay: React.FC = () => {
   const [title, setTitle] = useState<string>("로그인을 해주세요");
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      fetch("/api/lastTitle", {
-        // 사용자의 마지막 대화 타이틀을 조회하는 서버의 엔드포인트를 가정합니다.
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((data: TitleData) => {
-          if (data && data.title) {
-            setTitle(data.title); // 서버로부터 받은 마지막 대화 타이틀을 설정
-          }
-        })
-        .catch((error) => console.error("Error fetching last title:", error));
-    }
-  }, []);
+  // useEffect(() => {
+  //   const token = localStorage.getItem("token");
+  //   if (token) {
+  //     fetch("/api/lastTitle", {
+  //       // 사용자의 마지막 대화 타이틀을 조회하는 서버의 엔드포인트를 가정합니다.
+  //       method: "GET",
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data: TitleData) => {
+  //         if (data && data.title) {
+  //           setTitle(data.title); // 서버로부터 받은 마지막 대화 타이틀을 설정
+  //         }
+  //       })
+  //       .catch((error) => console.error("Error fetching last title:", error));
+  //   }
+  // }, []);
 
   return (
     <div className="inline-block mt-5 p-4 bg-white rounded-lg shadow-md">
