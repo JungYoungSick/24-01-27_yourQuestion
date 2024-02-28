f5c19ab5f61f38bd468d30d4b96a0c03be800adc
JungYoungSick <junfas1216@gmail.com>
2024-02-28 14:29:33 +0900

    해쉬를 통한 페이지 열기 완료

--- yourquestion/pages/Component/Src/Atoms/button/titleDisplay.tsx
+++ yourquestion/pages/Component/Src/Atoms/button/titleDisplay.tsx
@@ -1,37 +1,14 @@
-"use client";
-import React, { useState, useEffect } from "react";
+// TitleDisplay.tsx
+import React from "react";
 
-interface TitleData {
-  title: string;
+interface TitleDisplayProps {
+  title?: string;
 }
 
-const TitleDisplay: React.FC = () => {
-  const [title, setTitle] = useState<string>("로그인을 해주세요");
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-  //   if (token) {
-  //     fetch("/api/lastTitle", {
-  //       // 사용자의 마지막 대화 타이틀을 조회하는 서버의 엔드포인트를 가정합니다.
-  //       method: "GET",
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //     })
-  //       .then((response) => response.json())
-  //       .then((data: TitleData) => {
-  //         if (data && data.title) {
-  //           setTitle(data.title); // 서버로부터 받은 마지막 대화 타이틀을 설정
-  //         }
-  //       })
-  //       .catch((error) => console.error("Error fetching last title:", error));
-  //   }
-  // }, []);
-
+const TitleDisplay: React.FC<TitleDisplayProps> = ({ title }) => {
   return (
-    <div className="inline-block mt-5 p-4 bg-white rounded-lg shadow-md">
-      <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
+    <div className="title-display">
+      <h2>{title || "로그인을 해주세요."}</h2>
     </div>
   );
 };
---
+++ yourquestion/pages/Component/Src/interface/mainPageProps.tsx
@@ -0,0 +1,3 @@
+export interface MainPageProps {
+  talkTitleName: string;
+}
