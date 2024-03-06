7420915d6985abad86320b13c17c948ab8e5d5e4
JungYoungSick <junfas1216@gmail.com>
2024-03-06 14:22:54 +0900

    handleUserInput 코드 수정

--- yourquestion/pages/Component/Src/Molecules/handleUserInput.tsx
+++ yourquestion/pages/Component/Src/Molecules/handleUserInput.tsx
@@ -1,11 +1,13 @@
 // 사용자 입력을 처리하는 유틸리티 함수인 handleUserInput입니다. 이 함수는 다음과 같은 작업을 수행
 // 이러한 유틸리티 함수는 사용자 입력을 처리하고, 필요한 경우 팝업을 열거나 서버로 데이터를 전송하는 등의 작업을 수행합니다.
 export const handleUserInput = async (
+  setIsPopupOpen: React.Dispatch<React.SetStateAction<boolean>>,
   handleSubmit: () => Promise<void>
 ): Promise<void> => {
   const token = localStorage.getItem("token");
   if (!token) {
     alert("로그인이 필요합니다.");
+    setIsPopupOpen(true);
     return;
   }
   await handleSubmit();
